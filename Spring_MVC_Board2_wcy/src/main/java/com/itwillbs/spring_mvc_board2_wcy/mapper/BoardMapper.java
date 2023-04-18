7c2ed177e8575d4904962d0d4a08d9bc81345544
chanyoung <wsx6578@naver.com>
2023-04-18 15:05:30 +0900

    First Commit

---
+++ Spring_MVC_Board2_wcy/src/main/java/com/itwillbs/spring_mvc_board2_wcy/mapper/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.itwillbs.spring_mvc_board2_wcy.mapper;
+
+import com.itwillbs.spring_mvc_board2_wcy.vo.*;
+
+public interface BoardMapper {
+
+	// 글 쓰기
+	int insertBoard(BoardVO board);
+
+}
